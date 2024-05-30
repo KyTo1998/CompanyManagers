c281cbfb9c11f82e86578dedfd6d555ff01cbf8f
Tô Ký <ngockyto008@gmail.com>
2024-05-30 23:29:02 +0700

    Update_createCalendarWork

---
+++ CompanyManagers/Views/PageStaff/Proposing/pageCreateProposedWorkSchedule.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CompanyManagers.Views.PageStaff.Proposing
+{
+    /// <summary>
+    /// Interaction logic for pageCreateProposedWorkSchedule.xaml
+    /// </summary>
+    public partial class pageCreateProposedWorkSchedule : UserControl
+    {
+        public pageCreateProposedWorkSchedule()
+        {
+            InitializeComponent();
+        }
+
+        private void DeleteUserConfirmAll(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void DeleteFileGim(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void AddFileGimForProposing(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void DeleteUserConfirm(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
